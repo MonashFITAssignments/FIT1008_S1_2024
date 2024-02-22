66aec6a66041b76a842ad64b9f8df8a7ddaf4b24
Saksham Nagpal <sakshamnagpal@outlook.com>
2024-02-23 10:20:51 +1100

    implementing feedback

--- battle.py
+++ battle.py
@@ -1,20 +1,13 @@
 from poke_team import Trainer, PokeTeam
-from enum import Enum
 from data_structures.stack_adt import ArrayStack
 from data_structures.queue_adt import CircularQueue
 from data_structures.array_sorted_list import ArraySortedList
 from data_structures.sorted_list_adt import ListItem
 from typing import Tuple
+from battle_mode import BattleMode
 
-
-class BattleMode(Enum):
-    SET = 0
-    ROTATE = 1
-    OPTIMISE = 2
 
 class Battle:
-
-    CRITERION_LIST = ["health", "experience", "defence", "battle_power", "level"]
 
     def __init__(self, trainer_1: Trainer, trainer_2: Trainer, battle_mode: BattleMode, criterion = "health") -> None:
         raise NotImplementedError
@@ -34,8 +27,6 @@ class Battle:
     def optimise_battle(self) -> PokeTeam | None:
         raise NotImplementedError
 
-    def special(self, team: PokeTeam) -> None:
-        raise NotImplementedError
 
 if __name__ == '__main__':
     t1 = Trainer('Ash')
---
+++ battle_mode.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class BattleMode(Enum):
+    SET = 0
+    ROTATE = 1
+    OPTIMISE = 2
